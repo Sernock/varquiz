4b9736eb026820ba7bcf7589fd74003a56673e3a
Sernock <shprotokot@gmail.com>
2025-09-14 22:36:52 +0300

    database added

---
+++ api/database.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDB() {
+	var err error
+	DB, err = gorm.Open(sqlite.Open("app.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Ошибка подключения к базе данных", err)
+	}
+}
